9f88a7d5a8a7d913dffd2ffdd8f51c696ef0fe68
Juan Cerezo <7447739+juancerezo@users.noreply.github.com>
2024-04-01 10:40:44 +0000

    move mapping_object as visit argument to allow singleton

--- python_criteria/sqlalchemy.py
+++ python_criteria/sqlalchemy.py
@@ -7,53 +7,54 @@ from .visitor import BaseVisitor
 
 
 class SQLAlchemyVisitor(BaseVisitor):
-    def __init__(self, mapping_object: SQLAlchemyTable) -> None:
-        super().__init__(mapping_object)
+    def visit_eq(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field) == comparison.value
 
-    def visit_eq(self, comparison: BooleanClause):
-        return self._attr(comparison.field) == comparison.value
+    def visit_ne(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field) != comparison.value
 
-    def visit_ne(self, comparison: BooleanClause):
-        return self._attr(comparison.field) != comparison.value
+    def visit_lt(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field) < comparison.value
 
-    def visit_lt(self, comparison: BooleanClause):
-        return self._attr(comparison.field) < comparison.value
+    def visit_le(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field) <= comparison.value
 
-    def visit_le(self, comparison: BooleanClause):
-        return self._attr(comparison.field) <= comparison.value
+    def visit_gt(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field) > comparison.value
 
-    def visit_gt(self, comparison: BooleanClause):
-        return self._attr(comparison.field) > comparison.value
+    def visit_ge(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field) >= comparison.value
 
-    def visit_ge(self, comparison: BooleanClause):
-        return self._attr(comparison.field) >= comparison.value
+    def visit_in(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field).in_(comparison.value)
 
-    def visit_in(self, comparison: BooleanClause):
-        return self._attr(comparison.field).in_(comparison.value)
+    def visit_like(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field).ilike(
+            comparison.value, escape="\\"
+        )
 
-    def visit_like(self, comparison: BooleanClause):
-        return self._attr(comparison.field).ilike(comparison.value, escape="\\")
+    def visit_not_like(self, mapping_object: Any, comparison: BooleanClause):
+        return self._attr(mapping_object, comparison.field).not_ilike(
+            comparison.value, escape="\\"
+        )
 
-    def visit_not_like(self, comparison: BooleanClause):
-        return self._attr(comparison.field).not_ilike(comparison.value, escape="\\")
-
-    def visit_or(self, comparisons: list[Any]):
+    def visit_or(self, mapping_object: Any, comparisons: list[Any]):
         _op = comparisons[0]
         for comp in comparisons[1:]:
             _op = _op | comp  #! <--- Caution: do not modify bitwise operator
 
         return _op
 
-    def visit_and(self, comparisons: list[Any]):
+    def visit_and(self, mapping_object: Any, comparisons: list[Any]):
         _op = comparisons[0]
         for comp in comparisons[1:]:
             _op = _op & comp  #! <--- Caution: do not modify bitwise operator
         return _op
 
-    def visit_xor(self, comparisons: list[Any]):
+    def visit_xor(self, mapping_object: Any, comparisons: list[Any]):
         return (
             comparisons[0] ^ comparisons[1]
         )  #! <--- Caution: do not modify bitwise operator
 
-    def visit_not(self, comparisons: list[Any]):
+    def visit_not(self, mapping_object: Any, comparisons: list[Any]):
         return ~comparisons[0]  #! <--- Caution: do not modify bitwise operator
--- python_criteria/visitor.py
+++ python_criteria/visitor.py
@@ -6,11 +6,6 @@ from .filter import FilterableAttribute
 
 
 class BaseVisitor(metaclass=abc.ABCMeta):
-    mapping_object: Any
-
-    def __init__(self, mapping_object: Any) -> None:
-        self.mapping_object = mapping_object
-
     @classmethod
     def __subclasshook__(cls, subclass):
         return (
@@ -43,16 +38,17 @@ class BaseVisitor(metaclass=abc.ABCMeta):
             or NotImplemented
         )
 
-    def _attr(self, field: FilterableAttribute):
-        if not hasattr(self.mapping_object, field.name):
+    def _attr(self, mapping_object: Any, field: FilterableAttribute):
+        if not hasattr(mapping_object, field.name):
             raise ValueError(
                 f"'{field.name}' is not a valid attribute of '{field.parent_class.__name__}'"
             )
 
-        return getattr(self.mapping_object, field.name)
+        return getattr(mapping_object, field.name)
 
     def visit(
         self,
+        mapping_object: Any,
         filter_or_comparison: FilterableAttribute | BooleanClause | BooleanClauseList,
     ):
         name = filter_or_comparison.__class__.__name__.lower()
@@ -61,60 +57,60 @@ class BaseVisitor(metaclass=abc.ABCMeta):
         if isinstance(filter_or_comparison, BooleanClauseList):
             comparisons = []
             for _filter in filter_or_comparison.clause_list:
-                comparisons.append(self.visit(_filter))
+                comparisons.append(self.visit(mapping_object, _filter))
 
-            return method(comparisons)
+            return method(mapping_object, comparisons)
 
-        return method(filter_or_comparison)
+        return method(mapping_object, filter_or_comparison)
 
     @abc.abstractmethod
-    def visit_eq(self, comparison: BooleanClause):
+    def visit_eq(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_ne(self, comparison: BooleanClause):
+    def visit_ne(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_lt(self, comparison: BooleanClause):
+    def visit_lt(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_le(self, comparison: BooleanClause):
+    def visit_le(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_gt(self, comparison: BooleanClause):
+    def visit_gt(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_ge(self, comparison: BooleanClause):
+    def visit_ge(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_in(self, comparison: BooleanClause):
+    def visit_in(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_like(self, comparison: BooleanClause):
+    def visit_like(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_not_like(self, comparison: BooleanClause):
+    def visit_not_like(self, mapping_object: Any, comparison: BooleanClause):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_or(self, comparisons: list[Any]):
+    def visit_or(self, mapping_object: Any, comparisons: list[Any]):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_and(self, comparisons: list[Any]):
+    def visit_and(self, mapping_object: Any, comparisons: list[Any]):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_xor(self, comparisons: list[Any]):
+    def visit_xor(self, mapping_object: Any, comparisons: list[Any]):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def visit_not(self, comparisons: list[Any]):
+    def visit_not(self, mapping_object: Any, comparisons: list[Any]):
         raise NotImplementedError
